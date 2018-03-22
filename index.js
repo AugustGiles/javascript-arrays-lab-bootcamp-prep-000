6959c59ff325a198e86d6be7df563c5f1205e6a1
Taylor August Giles <augustgiles8@gmail.com>
2018-03-22 00:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = "I don't do much."
+const app = "I don't do much.";
 
 function kittens() {
   var kittens = ["Milo", "Otis", "Garfield"];
@@ -6,5 +6,33 @@ function kittens() {
 }
 
 function destructivelyAppendKitten(name) {
-  return kittens.unpush(name)
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unpush(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,2);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(-2);
 }
